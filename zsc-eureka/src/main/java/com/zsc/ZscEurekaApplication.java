36218aadc4f22a86605781f3c1af76b5ce0a22f0
Your Name <you@example.com>
2018-01-17 18:51:24 +0800

    config

--- zsc-eureka/src/main/java/com/zsc/ZscEurekaApplication.java
+++ zsc-eureka/src/main/java/com/zsc/ZscEurekaApplication.java
@@ -1,12 +1,20 @@
 package com.zsc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ZscEurekaApplication {
+	final static Logger logger = LoggerFactory.getLogger(ZscEurekaApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(ZscEurekaApplication.class, args);
+		ConfigurableApplicationContext applicationContext = new SpringApplicationBuilder(ZscEurekaApplication.class).web(true).run(args);
+		logger.debug("eureka已经启动,当前host：{}", applicationContext.getEnvironment().getProperty("HOSTNAME"));
 	}
 }
