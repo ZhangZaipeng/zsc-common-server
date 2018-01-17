f1d84907facea653370cd5ace901659077a9505b
Your Name <you@example.com>
2018-01-17 16:13:14 +0800

    Initial commit

---
+++ zsc-eureka/src/main/java/com/zsc/ZscEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.zsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZscEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZscEurekaApplication.class, args);
+	}
+}
